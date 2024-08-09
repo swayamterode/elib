5d90d95145330b11a8c69cc7c413d9e015421a4a
Swayam Terode <swayamterodex@gmail.com>
2024-08-10 02:57:26 +0530

    initial commit

---
+++ server.ts
@@ -0,0 +1,11 @@
+import express from "express";
+import app from "./src/app";
+import { config } from "./src/config/config";
+
+const startServer = async () => {
+  app.listen(config.port, () => {
+    console.log(`Server is running on port ${config.port}`);
+  });
+};
+
+startServer();
